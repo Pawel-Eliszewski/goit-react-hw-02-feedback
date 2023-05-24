e00f53a5f0758d9352b316e5f301e0066f0c05c8
Pawel-Eliszewski <eliszewskipawel@gmail.com>
2023-05-24 20:51:38 +0200

    small fix

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -14,7 +14,7 @@ export const Statistics = ({
       <li className={css.item}>Neutral: {neutral}</li>
       <li className={css.item}>Bad: {bad}</li>
       <li className={css.item}>Total: {total}</li>
-      <li className={css.item}>Positive feedback :{positivePercentage}%</li>
+      <li className={css.item}>Positive feedback: {positivePercentage}%</li>
     </ul>
   );
 };
